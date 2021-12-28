bd6b0db4f8bf0d7d6a30b17711379ae320844820
vanphan24 <89482663+vanphan24@users.noreply.github.com>
2021-12-28 12:42:56 -0800

    Delete eks/Deploy-consul/1.11GA/apps/countingapp directory

--- eks/Deploy-consul/1.11GA/apps/countingapp/delete-countingapp.sh
+++
@@ -1,7 +0,0 @@
-kubectl delete serviceaccount/counting 
-kubectl delete service/counting 
-kubectl delete deployment.apps/counting 
-kubectl delete serviceaccount/dashboard
-kubectl delete service/dashboard
-kubectl delete deployment.apps/dashboard 
-kubectl delete servicedefaults.consul.hashicorp.com/dashboard 
