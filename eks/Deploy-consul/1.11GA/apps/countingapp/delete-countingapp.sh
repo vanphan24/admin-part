41331dac39e6f66e42e81b0f5c0bf8a599ba1e8a
vanphan24 <89482663+vanphan24@users.noreply.github.com>
2021-12-28 12:41:28 -0800

    Add files via upload

---
+++ eks/Deploy-consul/1.11GA/apps/countingapp/delete-countingapp.sh
@@ -0,0 +1,7 @@
+kubectl delete serviceaccount/counting 
+kubectl delete service/counting 
+kubectl delete deployment.apps/counting 
+kubectl delete serviceaccount/dashboard
+kubectl delete service/dashboard
+kubectl delete deployment.apps/dashboard 
+kubectl delete servicedefaults.consul.hashicorp.com/dashboard 
---
+++ eks/Deploy-consul/1.11GA/apps/helloapp/delete-helloapp.sh
@@ -0,0 +1,8 @@
+kubectl delete servicedefaults.consul.hashicorp.com/static-client 
+kubectl delete serviceaccount/static-client 
+kubectl delete service/static-client 
+kubectl delete deployment.apps/static-client 
+kubectl delete servicedefaults.consul.hashicorp.com/static-server 
+kubectl delete serviceaccount/static-server 
+kubectl delete service/static-server 
+kubectl delete deployment.apps/static-server 
